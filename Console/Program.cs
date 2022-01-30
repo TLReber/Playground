83bf6781d5d152edad1a4577480daf2880eefd37
TLReber <mail@thomasreber.com>
2022-01-29 22:58:47 -0500

    Messing around with producer consumer channels.

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,30 +1,74 @@
-﻿using System.Threading.Channels;
+﻿namespace Console;
 
-var cts   = new CancellationTokenSource();
-var token = cts.Token;
+using System;
+using System.Threading.Channels;
 
-while (!token.IsCancellationRequested)
+public static class Program
 {
-    Console.WriteLine("Please enter a command - Q to quit.\n");
+    private static readonly Channel<int> _channel = Channel.CreateUnbounded<int>(
+        new UnboundedChannelOptions
+        {
+            SingleReader = true,
+            SingleWriter = true
+        }
+    );
 
-    var input = Console.ReadLine();
-
-    switch (input)
+    private static async Task Produce(CancellationToken token)
     {
-        case "q":
-        case "Q":
-            cts.Cancel();
-            break;
-        default:
-            Console.WriteLine($"{DateTime.Now:h:mm:ss.fff} - Processing next command in 1000ms.");
-            break;
+        while (!token.IsCancellationRequested)
+        {
+            var next = Random.Shared.Next(1, 5001);
+            // Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} - Writing {next,-5}");
+            await _channel.Writer.WriteAsync(next, token);
+
+            var delay = Random.Shared.NextDouble() * 2;
+            await Task.Delay(TimeSpan.FromSeconds(delay), token);
+        }
     }
 
-    try
+    private static async Task Consume(CancellationToken token)
     {
-        await Task.Delay(1000, cts.Token);
+        while (!token.IsCancellationRequested)
+        {
+            var next = await _channel.Reader.ReadAsync(token);
+            Console.WriteLine($"{DateTime.Now:mm:ss.fff} - Got {next,5}");
+        }
     }
-    catch (TaskCanceledException) { }
+
+    public static async Task Main()
+    {
+        var cts   = new CancellationTokenSource();
+        var token = cts.Token;
+
+        var producer = Task.Run(() => Produce(token), token);
+        var consumer = Task.Run(() => Consume(token), token);
+
+        while (!token.IsCancellationRequested)
+        {
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "q":
+                case "Q":
+                    cts.Cancel();
+                    break;
+            }
+
+            try
+            {
+                await Task.Delay(1000, cts.Token);
+            }
+            catch (TaskCanceledException) { }
+        }
+
+        try
+        {
+            await producer;
+            await consumer;
+        }
+        catch (TaskCanceledException) { }
+
+        Console.WriteLine("Goodbye!");
+    }
 }
-
-Console.WriteLine("Goodbye!");
