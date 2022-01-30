79c1e4f3b733edfad15b8b8aab4a3db8054c8642
TLReber <mail@thomasreber.com>
2022-01-29 22:32:09 -0500

    Init scratch commit.

---
+++ Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+var cts   = new CancellationTokenSource();
+var token = cts.Token;
+
+while (!token.IsCancellationRequested)
+{
+    Console.WriteLine("Please enter a command - Q to quit.\n");
+
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "q":
+        case "Q":
+            cts.Cancel();
+            break;
+        default:
+            Console.WriteLine($"{DateTime.Now:h:mm:ss.fff} - Processing next command in 1000ms.");
+            break;
+    }
+
+    try
+    {
+        await Task.Delay(1000, cts.Token);
+    }
+    catch (TaskCanceledException) { }
+}
+
+Console.WriteLine("Goodbye!");
